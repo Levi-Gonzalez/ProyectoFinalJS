d2b50fa7787b3a14a9e3232a5e89e81790412e7e
levi-gonzalez <levigonzalez.t@gmail.com>
2022-06-07 23:31:18 +0200

    proyecto final

---
+++ asset/productos.js
@@ -0,0 +1,39 @@
+// Mi array de objetos.
+const productos = [ 
+    {
+        nombre:"camiseta Argentina",
+        precio: 14.000,
+        imagenes: "./imagenes/argentina.jpg",
+        id: 1
+    },
+    {
+        nombre:"camiseta Argentina alternativa",
+        precio: 11.000,
+        imagenes: "./imagenes/arg-alt.jpg",
+        id: 2
+    },
+    {
+        nombre:"camiseta Boca",
+        precio: 10.000,
+        imagenes: "./imagenes/boca.jpg",
+        id: 3
+    },
+    {
+        nombre:"camiseta PSG",
+        precio: 9.000,
+        imagenes: "./imagenes/psg.jpg",
+        id: 4
+    },
+    {
+        nombre:"camiseta Barcelona",
+        precio: 8.500,
+        imagenes: "imagenes/barcelona.jpg",
+        id: 5
+    },
+    {
+        nombre:"camiseta Manchester city",
+        precio: 8.000,
+        imagenes: "./imagenes/manchester-city.jpg",
+        id: 6
+    },
+]
